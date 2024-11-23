f516df49002f56e98a725033522751549d3f508c
Matthew Mueller <mattmuelle@gmail.com>
2024-11-23 17:14:45 -0600

    support programmatically listing tags

--- chunky.go
+++ chunky.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"log/slog"
+	"sort"
 
 	"github.com/matthewmueller/chunky/internal/commits"
+	"github.com/matthewmueller/chunky/internal/tags"
 	"github.com/matthewmueller/chunky/repos"
 )
 
@@ -41,4 +43,43 @@ func (c *Client) FindCommit(ctx context.Context, in *FindCommit) (*Commit, error
 		return nil, err
 	}
 	return commits.Read(ctx, in.Repo, in.Revision)
+}
+
+type ListTags struct {
+	Repo repos.Repo
+}
+
+func (in *ListTags) validate() (err error) {
+	if in.Repo == nil {
+		err = errors.Join(err, errors.New("missing 'repo'"))
+	}
+	return err
+}
+
+type Tag struct {
+	Name    string
+	Commits []string
+}
+
+func (c *Client) ListTags(ctx context.Context, in *ListTags) (allTags []*Tag, err error) {
+	if err := in.validate(); err != nil {
+		return nil, err
+	}
+	commitToTags, err := tags.ReadMap(ctx, in.Repo)
+	if err != nil {
+		return nil, err
+	}
+	tagMap := map[string][]string{}
+	for commit, tag := range commitToTags {
+		for _, tag := range tag {
+			tagMap[tag] = append(tagMap[tag], commit)
+		}
+	}
+	for tag, commits := range tagMap {
+		allTags = append(allTags, &Tag{tag, commits})
+	}
+	sort.Slice(allTags, func(i, j int) bool {
+		return allTags[i].Name < allTags[j].Name
+	})
+	return allTags, nil
 }
